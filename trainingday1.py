a45b7f0e4ae4ab09cc44f5ddbff543d98c0dd7ba
KanuckEO <76070714+KanuckEO@users.noreply.github.com>
2021-03-02 10:20:38 -0500

    Add files via upload

---
+++ trainingday1.py
@@ -0,0 +1,12 @@
+#print
+print ("Hi, I am stupid.")
+#user input
+#KSKS
+ans = input("hi, how you")
+if ans == 'gud':
+    print("noice")
+#print the same thing 100 times with one print statment
+#omastar
+print (100*"Hi, ya stupid.")
+
+
